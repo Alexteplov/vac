1be8b8dba375ec656c8884f143e3f71a38b673fc
Alexteplov <teplov0@gmail.com>
2012-09-28 11:06:01 +0600

    add count of people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
   # GET /people.json
   def index
     @people = Person.order('id desc')
-
+    @people_count = Person.count
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @people }
